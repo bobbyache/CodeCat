f82cdd3e78263106d7b3725741c1a2d41b23ab4f
bobyache <robbie.a.blake@gmail.com>
2016-05-13 09:35:57 +0200

    Initial ProjectConverter created with empty but workable unit test framework.

---
+++ Code/Components/CygSoft.CodeCat.ProjectConverter/ToVersion2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CygSoft.CodeCat.ProjectConverter
+{
+    public class ToVersion2
+    {
+        internal XElement ConvertIndex(XElement previousVersion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code/UnitTests/TestBench.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTestFile
+{
+    [TestClass]
+    [DeploymentItem(@"Files\ProjectFiles\CodeIndex V1.xml")]
+    [DeploymentItem(@"Files\ProjectFiles\CodeIndex V2.xml")]
+    public class TestBench
+    {
+        //[TestMethod]
+        //public void MyTest()
+        //{
+        //    //Assert.IsTrue(File.Exists(@"helloworld.txt"));
+
+        //    string version1_Path = "CodeIndex V1.xml";
+        //    string version2_Path = "CodeIndex V2.xml";
+
+        //    using (StreamReader version1Reader = new StreamReader(version1_Path))
+        //    {
+        //        string text = version1Reader.ReadToEnd();
+        //    }
+
+
+        //    using (StreamReader version2Reader = new StreamReader(version2_Path))
+        //    {
+        //        string text = version2Reader.ReadToEnd();
+        //    }
+        //}
+
+        [TestMethod]
+        public void ConvertIndexItem_Version1_to_Version2()
+        {
+
+        }
+    }
+}
