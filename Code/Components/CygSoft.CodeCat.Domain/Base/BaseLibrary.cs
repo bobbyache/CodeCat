207fbe08eca651cae40390da42b065593d66a7f2
bobyache <robbie.a.blake@gmail.com>
2016-07-29 16:44:02 +0200

    Fixed BaseLibrary namespace.

--- Code/Components/CygSoft.CodeCat.Domain/Base/BaseLibrary.cs
+++ Code/Components/CygSoft.CodeCat.Domain/Base/BaseLibrary.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CygSoft.CodeCat.Domain
+namespace CygSoft.CodeCat.Domain.Base
 {
     internal abstract class BaseLibrary
     {
