48e3ef45b8c3701f11e3515e195eca9ed68b643a
bobyache <robbie.a.blake@gmail.com>
2016-09-16 17:22:05 +0200

    Added Moq framework to CodeCat's UnitTests.DocumentManager.csproj

---
+++ Code/Components/UnitTests.DocumentManager/DocumentIndex_FileInfo_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.DocumentManager
+{
+    [TestClass]
+    public class DocumentIndex_FileInfo_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
