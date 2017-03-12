0c9b29977ed29be68b19e76f5857cbcd76a01705
Rob Blake <robbie.a.blake@gmail.com>
2017-03-12 09:17:33 +0200

    Added DocumentManager unit test project.

---
+++ Code/UnitTests/DocumentManager.UnitTests/TestBench.cs
@@ -0,0 +1,24 @@
+﻿using CygSoft.CodeCat.DocumentManager;
+using CygSoft.CodeCat.DocumentManager.Documents;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManager.UnitTests
+{
+    [TestFixture]
+    public class TestBench
+    {
+        [Test]
+        public void CodeDocument_Test()
+        {
+            CodeDocument codeDocument = DocumentFactory.Create(CygSoft.CodeCat.DocumentManager.Infrastructure.DocumentTypeEnum.CodeSnippet);
+            //codeDocument.Open()
+
+            Assert.Fail("This is a test method that must fail.");
+        }
+    }
+}
