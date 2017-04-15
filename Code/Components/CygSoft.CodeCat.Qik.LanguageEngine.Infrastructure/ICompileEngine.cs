ce787a45e15fe97695d7ccbd7f2dcebaa90b7650
Rob Blake <robbie.a.blake@gmail.com>
2017-04-15 09:27:16 +0200

    Added Qik.Infrastructure and the empty project template for the qik engine.

---
+++ Code/Components/CygSoft.CodeCat.Qik.LanguageEngine.Infrastructure/ICompileEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CygSoft.CodeCat.Qik.LanguageEngine.Infrastructure
+{
+    public interface ICompileEngine
+    {
+        event EventHandler BeforeInput;
+        event EventHandler AfterInput;
+        event EventHandler BeforeCompile;
+        event EventHandler AfterCompile;
+
+        string[] Placeholders { get; }
+        string[] Symbols { get; }
+        IInputField[] InputFields { get; }
+        IExpression[] Expressions { get; }
+
+        void CreateFieldInput(string symbol, string fieldName, string description);
+
+        void Compile(string scriptText);
+        void Input(string symbol, string value);
+
+        ISymbolInfo GetSymbolInfo(string symbol);
+        ISymbolInfo GetPlaceholderInfo(string placeholder);
+        ISymbolInfo[] GetSymbolInfoSet(string[] symbols);
+        string GetValueOfSymbol(string symbol);
+        string GetValueOfPlaceholder(string placeholder);
+        string GetTitleOfPlaceholder(string placeholder);
+    }
+}
