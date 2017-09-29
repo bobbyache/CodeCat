b954853b5016200666c83cc22484469bcbb8e67b
Rob Blake <rob_bl8ke@hotmail.com>
2017-09-29 13:34:09 +0200

    Working with multiple plugins.

---
+++ Code/Components/CygSoft.CodeCat.Plugins.ManualXess/ManualXessGenerator.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CygSoft.CodeCat.Plugins.ManualXess
+{
+    partial class ManualXessGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCaption.Location = new System.Drawing.Point(0, 0);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(309, 315);
+            this.lblCaption.TabIndex = 1;
+            this.lblCaption.Text = "label1";
+            this.lblCaption.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ManualXessGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblCaption);
+            this.Name = "ManualXessGenerator";
+            this.Size = new System.Drawing.Size(309, 315);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCaption;
+    }
+}
---
+++ Code/Components/CygSoft.CodeCat.Plugins.ManualXess/ManualXessGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CygSoft.CodeCat.Plugins.Generators;
+
+namespace CygSoft.CodeCat.Plugins.ManualXess
+{
+    public partial class ManualXessGenerator: UserControl, IGeneratorPlugin
+    {
+        public ManualXessGenerator()
+        {
+            InitializeComponent();
+            lblCaption.Text = this.Title;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return "ManualXessGenerator";
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return "Quick Adhoc Batch Generator";
+            }
+        }
+
+        public event EventHandler Generated;
+    }
+}
