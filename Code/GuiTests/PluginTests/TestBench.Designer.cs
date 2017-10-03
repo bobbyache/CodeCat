277e6f41045c24f5526338ee8ace5af4e08dcd19
Rob Blake <rob_bl8ke@hotmail.com>
2017-10-03 10:43:20 +0200

    Created the Plugins TestBench project.

---
+++ Code/GuiTests/PluginTests/TestBench.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CygSoft.CodeCat.PluginTests
+{
+    partial class TestBench
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pluginMenu = new System.Windows.Forms.MenuStrip();
+            this.mnuPlugins = new System.Windows.Forms.ToolStripMenuItem();
+            this.pluginPanel = new System.Windows.Forms.Panel();
+            this.mnuXessManual = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSqlToCsString = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtCurrentPlugin = new System.Windows.Forms.ToolStripTextBox();
+            this.pluginMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pluginMenu
+            // 
+            this.pluginMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuPlugins,
+            this.txtCurrentPlugin});
+            this.pluginMenu.Location = new System.Drawing.Point(0, 0);
+            this.pluginMenu.Name = "pluginMenu";
+            this.pluginMenu.Size = new System.Drawing.Size(640, 27);
+            this.pluginMenu.TabIndex = 0;
+            this.pluginMenu.Text = "menuStrip1";
+            // 
+            // mnuPlugins
+            // 
+            this.mnuPlugins.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuXessManual,
+            this.mnuSqlToCsString});
+            this.mnuPlugins.Name = "mnuPlugins";
+            this.mnuPlugins.Size = new System.Drawing.Size(58, 23);
+            this.mnuPlugins.Text = "Plugins";
+            // 
+            // pluginPanel
+            // 
+            this.pluginPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pluginPanel.Location = new System.Drawing.Point(12, 27);
+            this.pluginPanel.Name = "pluginPanel";
+            this.pluginPanel.Size = new System.Drawing.Size(616, 333);
+            this.pluginPanel.TabIndex = 1;
+            // 
+            // mnuXessManual
+            // 
+            this.mnuXessManual.Name = "mnuXessManual";
+            this.mnuXessManual.Size = new System.Drawing.Size(227, 22);
+            this.mnuXessManual.Text = "Xess Manual Generator";
+            // 
+            // mnuSqlToCsString
+            // 
+            this.mnuSqlToCsString.Name = "mnuSqlToCsString";
+            this.mnuSqlToCsString.Size = new System.Drawing.Size(227, 22);
+            this.mnuSqlToCsString.Text = "Unformatted Sql to C# String";
+            // 
+            // txtCurrentPlugin
+            // 
+            this.txtCurrentPlugin.Enabled = false;
+            this.txtCurrentPlugin.Name = "txtCurrentPlugin";
+            this.txtCurrentPlugin.Size = new System.Drawing.Size(400, 23);
+            // 
+            // TestBench
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 372);
+            this.Controls.Add(this.pluginPanel);
+            this.Controls.Add(this.pluginMenu);
+            this.MainMenuStrip = this.pluginMenu;
+            this.Name = "TestBench";
+            this.Text = "Plugins Test Bench";
+            this.pluginMenu.ResumeLayout(false);
+            this.pluginMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip pluginMenu;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlugins;
+        private System.Windows.Forms.Panel pluginPanel;
+        private System.Windows.Forms.ToolStripMenuItem mnuXessManual;
+        private System.Windows.Forms.ToolStripMenuItem mnuSqlToCsString;
+        private System.Windows.Forms.ToolStripTextBox txtCurrentPlugin;
+    }
+}
+
---
+++ Code/GuiTests/PluginTests/TestBench.cs
@@ -0,0 +1,87 @@
+﻿using CygSoft.CodeCat.Plugins.Generators;
+using CygSoft.CodeCat.Plugins.ManualXess;
+using CygSoft.CodeCat.Plugins.SqlExtraction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CygSoft.CodeCat.PluginTests
+{
+    public partial class TestBench : Form
+    {
+        private Dictionary<string, UserControl> pluginsDictionary = new Dictionary<string, UserControl>();
+
+        public TestBench()
+        {
+            InitializeComponent();
+
+            mnuSqlToCsString.Click += PluginMenu_Click;
+            mnuXessManual.Click += PluginMenu_Click;
+        }
+
+        private void PluginMenu_Click(object sender, EventArgs e)
+        {
+            UserControl userControl = null;
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+            string menuName = menuItem.Name;
+
+            switch (menuName)
+            {
+                case "mnuXessManual":
+                    if (!pluginsDictionary.ContainsKey(menuName))
+                        pluginsDictionary.Add(menuName, new ManualXessGenerator());
+                    userControl = pluginsDictionary[menuName];
+                    break;
+
+                case "mnuSqlToCsString":
+                    if (!pluginsDictionary.ContainsKey(menuName))
+                        pluginsDictionary.Add(menuName, new SqlToCSharpStringGenerator());
+                    userControl = pluginsDictionary[menuName];
+                    break;
+            }
+
+            if (userControl != null)
+            {
+                DisplayPlugin(menuName, userControl);
+                UncheckMenus();
+                CheckMenu(menuItem);
+            }
+        }
+
+        private void UncheckMenus()
+        {
+            foreach (ToolStripMenuItem item in mnuPlugins.DropDownItems)
+            {
+                item.Checked = false;
+            }
+        }
+
+        private void CheckMenu(ToolStripMenuItem menu)
+        {
+            menu.Checked = true;
+        }
+
+        private void DisplayPlugin(string name, UserControl userControl)
+        {
+
+            this.pluginPanel.Controls.Clear();
+            this.pluginPanel.Controls.Add(userControl);
+            userControl.Dock = DockStyle.Fill;
+
+            txtCurrentPlugin.Text = ((IGeneratorPlugin)userControl).Title;
+
+            foreach (ToolStripItem item in pluginMenu.Items)
+            {
+                if (!(item is ToolStripTextBox))
+                    ((ToolStripMenuItem)item).Checked = item.Name == txtCurrentPlugin.Text ? true : false;
+            }
+
+        }
+    }
+}
