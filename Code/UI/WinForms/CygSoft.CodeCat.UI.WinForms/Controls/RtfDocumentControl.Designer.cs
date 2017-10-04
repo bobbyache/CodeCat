6019caa6679a0407f7ba3f4d9aedc8eb9b254d18
Rob Blake <rob_bl8ke@hotmail.com>
2017-10-04 12:10:20 +0200

    RichTextBox control (topic section) now inherits from BaseTopicSectionControl. Code simplified and in the process fixed: Bug #108 - RTF Editor Tab Title only displays a single letter (first letter of the title)

--- Code/UI/WinForms/CygSoft.CodeCat.UI.WinForms/Controls/RtfDocumentControl.Designer.cs
+++ Code/UI/WinForms/CygSoft.CodeCat.UI.WinForms/Controls/RtfDocumentControl.Designer.cs
@@ -28,93 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.txtTitle = new CygSoft.CodeCat.UI.WinForms.ToolStripSpringTextBox();
-            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
-            this.lblEditStatus = new System.Windows.Forms.ToolStripLabel();
             this.rtfEditor = new CygSoft.CodeCat.UI.WinForms.Controls.RtfEditorControl();
-            this.toolStrip1.SuspendLayout();
-            this.toolStrip2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripLabel1,
-            this.txtTitle});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(716, 25);
-            this.toolStrip1.TabIndex = 12;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // toolStripLabel1
-            // 
-            this.toolStripLabel1.AutoSize = false;
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(65, 22);
-            this.toolStripLabel1.Text = "Title";
-            this.toolStripLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // txtTitle
-            // 
-            this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(608, 25);
-            // 
-            // toolStrip2
-            // 
-            this.toolStrip2.AutoSize = false;
-            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.lblEditStatus});
-            this.toolStrip2.Location = new System.Drawing.Point(0, 520);
-            this.toolStrip2.Name = "toolStrip2";
-            this.toolStrip2.Size = new System.Drawing.Size(716, 25);
-            this.toolStrip2.TabIndex = 13;
-            this.toolStrip2.Text = "toolStrip2";
-            // 
-            // lblEditStatus
-            // 
-            this.lblEditStatus.AutoSize = false;
-            this.lblEditStatus.Name = "lblEditStatus";
-            this.lblEditStatus.Size = new System.Drawing.Size(100, 22);
-            this.lblEditStatus.Text = "No Changes";
-            this.lblEditStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // rtfEditor
             // 
             this.rtfEditor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtfEditor.Location = new System.Drawing.Point(0, 25);
+            this.rtfEditor.Location = new System.Drawing.Point(0, 0);
             this.rtfEditor.Name = "rtfEditor";
-            this.rtfEditor.Size = new System.Drawing.Size(716, 495);
+            this.rtfEditor.Size = new System.Drawing.Size(716, 545);
             this.rtfEditor.TabIndex = 14;
+            this.rtfEditor.TextRtf = "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang7177{\\fonttbl{\\f0\\fnil\\fcharset0 Calibri;}}\r" +
+    "\n\\viewkind4\\uc1\\pard\\f0\\fs24\\par\r\n}\r\n";
             // 
             // RtfDocumentControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.rtfEditor);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.toolStrip2);
             this.Name = "RtfDocumentControl";
             this.Size = new System.Drawing.Size(716, 545);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            this.toolStrip2.ResumeLayout(false);
-            this.toolStrip2.PerformLayout();
+            this.Controls.SetChildIndex(this.rtfEditor, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private ToolStripSpringTextBox txtTitle;
-        private System.Windows.Forms.ToolStrip toolStrip2;
-        private System.Windows.Forms.ToolStripLabel lblEditStatus;
         private RtfEditorControl rtfEditor;
     }
 }
--- Code/UI/WinForms/CygSoft.CodeCat.UI.WinForms/Controls/RtfDocumentControl.cs
+++ Code/UI/WinForms/CygSoft.CodeCat.UI.WinForms/Controls/RtfDocumentControl.cs
@@ -4,44 +4,29 @@ using System.Windows.Forms;
 using CygSoft.CodeCat.Domain;
 using CygSoft.CodeCat.DocumentManager.Infrastructure;
 using CygSoft.CodeCat.Domain.Topics;
+using CygSoft.CodeCat.UI.WinForms.Controls.TopicSections;
 
 namespace CygSoft.CodeCat.UI.WinForms.Controls
 {
-    public partial class RtfDocumentControl : UserControl, ITopicSectionBaseControl
+    public partial class RtfDocumentControl : BaseTopicSectionControl
     {
-        public event EventHandler Modified;
+        public override int ImageKey { get { return IconRepository.Get(IconRepository.TopicSections.RTF).Index; } }
+        public override Icon ImageIcon { get { return IconRepository.Get(IconRepository.TopicSections.RTF).Icon; } }
+        public override Image IconImage { get { return IconRepository.Get(IconRepository.TopicSections.RTF).Image; } }
 
-        private IRichTextEditorTopicSection topicSection;
-        private ITopicDocument topicDocument;
-
-        public string Id { get; private set; }
-
-        public string Title
+        public RtfDocumentControl()
+            : this(null, null, null)
         {
-            get { return txtTitle.Text; }
+
         }
 
-        public int ImageKey { get { return IconRepository.Get(IconRepository.TopicSections.RTF).Index; } }
-        public Icon ImageIcon { get { return IconRepository.Get(IconRepository.TopicSections.RTF).Icon; } }
-        public Image IconImage { get { return IconRepository.Get(IconRepository.TopicSections.RTF).Image; } }
-
-        public bool IsModified { get; private set; }
-
-        public bool FileExists { get { return false; } }
-
         public RtfDocumentControl(AppFacade application, ITopicDocument topicDocument, IRichTextEditorTopicSection topicSection)
+            : base(application, topicDocument, topicSection)
         {
             InitializeComponent();
-            this.topicSection = topicSection;
-            this.topicDocument = topicDocument;
-            Id = topicSection.Id;
 
-            ResetFieldValues();
             LoadIfExists();
 
-            txtTitle.TextChanged += (s, e) => SetModified();
-            topicDocument.BeforeSave += codeGroupDocumentSet_BeforeContentSaved;
-            topicDocument.AfterSave += codeGroupDocumentSet_ContentSaved;
             topicSection.RequestSaveRtf += rtfDocument_RequestSaveRtf;
             rtfEditor.ContentChanged += rtfEditor_ContentChanged;
         }
@@ -49,7 +34,7 @@ namespace CygSoft.CodeCat.UI.WinForms.Controls
         private void rtfEditor_ContentChanged(object sender, EventArgs e)
         {
             if (rtfEditor.Modified)
-                SetModified();
+                Modify();
         }
 
         private void rtfDocument_RequestSaveRtf(object sender, EventArgs e)
@@ -57,48 +42,10 @@ namespace CygSoft.CodeCat.UI.WinForms.Controls
             rtfEditor.Save(topicSection.FilePath);
         }
 
-        public void Revert()
-        {
-            LoadIfExists();
-        }
-
-        private void SetModified()
-        {
-            if (!IsModified)
-            {
-                IsModified = true;
-                SetChangeStatus();
-                Modified?.Invoke(this, new EventArgs());
-            }
-        }
-
         private void LoadIfExists()
         {
             if (topicSection.Exists)
                 rtfEditor.OpenFile(topicSection.FilePath);
-        }
-
-        private void ResetFieldValues()
-        {
-            txtTitle.Text = topicSection.Title;
-            IsModified = false;
-        }
-
-        private void codeGroupDocumentSet_ContentSaved(object sender, TopicEventArgs e)
-        {
-            IsModified = false;
-            SetChangeStatus();
-        }
-
-        private void codeGroupDocumentSet_BeforeContentSaved(object sender, TopicEventArgs e)
-        {
-            topicSection.Title = txtTitle.Text;
-        }
-
-        private void SetChangeStatus()
-        {
-            lblEditStatus.Text = IsModified ? "Edited" : "Saved";
-            lblEditStatus.ForeColor = IsModified ? Color.DarkRed : Color.Black;
         }
     }
 }
