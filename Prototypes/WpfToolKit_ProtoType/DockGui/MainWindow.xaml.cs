0ed86e7b64814144124c11442481de54f1e46e7e
Rob Blake <rob_bl8ke@hotmail.com>
2017-12-13 11:34:39 +0200

    Basic documents are being added and removed through the menu, and the first events are explored.

--- Prototypes/WpfToolKit_ProtoType/DockGui/MainWindow.xaml.cs
+++ Prototypes/WpfToolKit_ProtoType/DockGui/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.AvalonDock.Layout;
 
 namespace DockGui
 {
@@ -23,6 +24,24 @@ namespace DockGui
         public MainWindow()
         {
             InitializeComponent();
+
+            layoutRoot.ElementAdded += (s, e) => { AddEvent($"{(e.Element as LayoutDocument).Title} added"); };
+            layoutRoot.ElementRemoved += (s, e) => { AddEvent($"{(e.Element as LayoutDocument).Title} removed"); };
+        }
+
+        private void AddEvent(string eventText)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = eventText;
+            ListBoxItem listBoxItem = new ListBoxItem();
+            listBoxItem.Content = textBlock;
+
+            EventsListBox.Items.Add(listBoxItem);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentArea.Children.Add(new LayoutDocument() { Title = Guid.NewGuid().ToString() });
         }
     }
 }
