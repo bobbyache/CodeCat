03ffa748ec4b1ef74f10eccac4a4164ba48ff2b3
Rob Blake <rob_bl8ke@hotmail.com>
2017-12-13 12:48:23 +0200

    IsActivated, IsSelected, and setting the newly added document to have focus.

--- Prototypes/WpfToolKit_ProtoType/DockGui/MainWindow.xaml.cs
+++ Prototypes/WpfToolKit_ProtoType/DockGui/MainWindow.xaml.cs
@@ -44,7 +44,10 @@ namespace DockGui
                 //AddEvent($"{(e.Element as LayoutDocumentFloatingWindow).RootDocument.Title} added");
             }
             else if (e.Element is LayoutDocument)
+            {
                 AddEvent($"{(e.Element as LayoutDocument).Title} added");
+                (e.Element as LayoutDocument).IsSelected = true;
+            }
         }
 
         private void AddEvent(string eventText)
@@ -59,7 +62,32 @@ namespace DockGui
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            DocumentArea.Children.Add(new LayoutDocument() { Title = Guid.NewGuid().ToString() });
+            LayoutDocument document = new LayoutDocument() { Title = Guid.NewGuid().ToString() };
+            document.IsSelectedChanged += Document_IsSelectedChanged;
+            document.IsActiveChanged += Document_IsActiveChanged;
+            DocumentArea.Children.Add(document);
+
+            
+        }
+
+        private void Document_IsActiveChanged(object sender, EventArgs e)
+        {
+            LayoutDocument document = sender as LayoutDocument;
+            if (document.IsActive)
+            {
+                AddEvent($"{ document.Title} activated");
+                ActiveDocumentStatus.Text = layoutRoot.LastFocusedDocument.Title;
+            }
+        }
+
+        private void Document_IsSelectedChanged(object sender, EventArgs e)
+        {
+            LayoutDocument document = sender as LayoutDocument;
+            if (document.IsSelected)
+            {
+                AddEvent($"{ document.Title} selected");
+                ActiveDocumentStatus.Text = layoutRoot.LastFocusedDocument.Title;
+            }
         }
     }
 }
