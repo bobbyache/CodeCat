894dfd2decfed2c6d45114ab0db9e9af727c29c2
Rob Blake <robbie.a.blake@gmail.com>
2018-06-08 16:51:18 +0200

    Removed ITopicSection.

--- Code/Components/CygSoft.CodeCat.Plugin.Infrastructure/PluginInterface.cs
+++ Code/Components/CygSoft.CodeCat.Plugin.Infrastructure/PluginInterface.cs
@@ -3,8 +3,39 @@ using System.Windows.Forms;
 
 namespace CygSoft.CodeCat.Plugin.Infrastructure
 {
-    public interface IPluginControl: ITopicSection
+    public interface IPluginControl: IPositionedItem
     {
+        event EventHandler<FileEventArgs> BeforeDelete;
+        event EventHandler<FileEventArgs> AfterDelete;
+
+        event EventHandler<FileEventArgs> BeforeOpen;
+        event EventHandler<FileEventArgs> AfterOpen;
+        event EventHandler<FileEventArgs> BeforeSave;
+        event EventHandler<FileEventArgs> AfterSave;
+        event EventHandler<FileEventArgs> BeforeClose;
+        event EventHandler<FileEventArgs> AfterClose;
+        event EventHandler<FileEventArgs> BeforeRevert;
+        event EventHandler<FileEventArgs> AfterRevert;
+
+        string FilePath { get; }
+        string FileName { get; }
+        string FileExtension { get; }
+        string Folder { get; }
+        bool FolderExists { get; }
+        bool Exists { get; }
+        bool Loaded { get; }
+
+        void Open();
+        void Delete();
+        void Save();
+        void Close();
+        void Revert();
+
+        string Id { get; }
+
+        string Title { get; set; }
+        string DocumentType { get; set; }
+        string Description { get; set; }
     }
 
 	public interface IPlugin
