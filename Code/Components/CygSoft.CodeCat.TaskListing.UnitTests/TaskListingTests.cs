063e1212173747bbadfae2286f5f9a493b0dabbf
bobbyache <robbie.a.blake@gmail.com>
2019-05-31 08:43:06 +0200

    Added unit test project.

---
+++ Code/Components/CygSoft.CodeCat.TaskListing.UnitTests/TaskListingTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.CodeCat.TaskListing.UnitTests
+{
+    [TestFixture]
+    public class TaskListingTests
+    {
+        [Test]
+        public void Test()
+        {
+            var taskListing = new TaskList("");
+            Assert.Fail();
+        }
+    }
+}
