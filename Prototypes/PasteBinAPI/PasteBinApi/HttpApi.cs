05e1e5d7a9b8c76b4502680fc4b195ab643efbd5
bobbyache <robbie.a.blake@gmail.com>
2021-05-06 13:49:36 +0200

    Boilerplate PasteBin API prototype start

---
+++ Prototypes/PasteBinAPI/PasteBinApi/HttpApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace PasteBinApi
+{
+    public class HttpApi
+    {
+        // public string GetPaste()
+        // {
+        //     var client = new HttpClient();
+        //     var params = new 
+
+        //     var request = new HttpRequestMessage
+        //     {
+        //         RequestUri = new Uri(@"https://pastebin.com/api/api_raw.php"),
+        //         Method = HttpMethod.Post,
+                
+
+        //     };
+        // }
+    }
+}
---
+++ Prototypes/PasteBinAPI/README.md
@@ -0,0 +1,14 @@
+## Get up and Running
+
+### Run the Console
+To run the console application only the `dotnet run` command is necessary unless running for the first time.
+
+```
+dotnet clean
+dotnet restore
+```
+
+Run the tests
+```
+dotnet test ./PasteBinApiTests/PasteBinApiTests.csproj
+```
